14b85c1a4f87ad518343f38e5233893142d874b0
Michael Matus <michael@matus.at>
2022-03-20 23:10:31 +0100

    Add new class Commander

---
+++ Commander.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Bev.Instruments.Heidenhain
+{
+    public class Commander
+    {
+        private static byte CtrlB = 0x02;
+        private static byte CR = 0x13;
+        private static byte LF = 0x10;
+        private static byte ESC = 0x1B;
+
+        public byte[] ACommand(int cmd) => StringToBytes(BuildCommandString('A', cmd));
+        public byte[] TCommand(int cmd) => StringToBytes(BuildCommandString('T', cmd));
+        public byte[] SCommand(int cmd) => StringToBytes(BuildCommandString('S', cmd));
+        public byte[] FCommand(int cmd) => StringToBytes(BuildCommandString('F', cmd));
+
+        private string BuildCommandString(char key, int cmd) => $"{ESC}{key}{IntTo4String(cmd)}{CR}"; // or escape sequence?
+
+        private string IntTo4String(int n)
+        {
+            if (n < 0) n = 0;
+            if (n > 9999) n = 0;
+            return n.ToString("D4");
+        }
+
+        private byte[] StringToBytes(string str)
+        {
+            Encoding enc = Encoding.ASCII;
+            byte[] bytes = enc.GetBytes(str);
+            return bytes;
+        }
+
+        public string HexBytesToString(byte[] bytes)
+        {
+            string hexString = BitConverter.ToString(bytes);
+            return hexString.Replace('-', ' '); // or leave it as is?
+        }
+    }
+}
--- ND280.cs
+++ ND280.cs
@@ -6,24 +6,5 @@ namespace Bev.Instruments.Heidenhain
         public ND280(string portName) : base(portName) { }
         public override string InstrumentType => "ND280";
 
-
-        private static byte CtrlB = 0x02;
-        private static byte CR = 0x13;
-        private static byte LF = 0x10;
-        private static byte ESC = 0x1B;
-
-        private byte[] GenerateRemoteCommandA()
-        {
-            byte[] command = new byte[7];
-            command[0] = ESC;
-            command[1] = 0x41;
-            command[2] = 0x30;
-            command[3] = 0x30;
-            command[4] = 0x30;
-            command[5] = 0x30;
-            command[6] = CR;
-            return command;
-        }
-
     }
 }
