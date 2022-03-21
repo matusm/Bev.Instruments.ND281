c4a2dc934bf002e45c4c3430297b01a362d2b5ae
Michael Matus <michael@matus.at>
2022-03-21 07:05:07 +0100

    Make Commander static

--- Commander.cs
+++ Commander.cs
@@ -3,35 +3,35 @@ using System.Text;
 
 namespace Bev.Instruments.Heidenhain
 {
-    public class Commander
+    public static class Commander
     {
         private static byte CtrlB = 0x02;
         private static byte CR = 0x13;
         private static byte LF = 0x10;
         private static byte ESC = 0x1B;
 
-        public byte[] ACommand(int cmd) => StringToBytes(BuildCommandString('A', cmd));
-        public byte[] TCommand(int cmd) => StringToBytes(BuildCommandString('T', cmd));
-        public byte[] SCommand(int cmd) => StringToBytes(BuildCommandString('S', cmd));
-        public byte[] FCommand(int cmd) => StringToBytes(BuildCommandString('F', cmd));
+        public static byte[] ACommand(int cmd) => StringToBytes(BuildCommandString('A', cmd));
+        public static byte[] TCommand(int cmd) => StringToBytes(BuildCommandString('T', cmd));
+        public static byte[] SCommand(int cmd) => StringToBytes(BuildCommandString('S', cmd));
+        public static byte[] FCommand(int cmd) => StringToBytes(BuildCommandString('F', cmd));
 
-        private string BuildCommandString(char key, int cmd) => $"{ESC}{key}{IntTo4String(cmd)}{CR}"; // or escape sequence?
+        private static string BuildCommandString(char key, int cmd) => $"{ESC}{key}{IntTo4String(cmd)}{CR}"; // or escape sequence?
 
-        private string IntTo4String(int n)
+        private static string IntTo4String(int n)
         {
             if (n < 0) n = 0;
             if (n > 9999) n = 0;
             return n.ToString("D4");
         }
 
-        private byte[] StringToBytes(string str)
+        private static byte[] StringToBytes(string str)
         {
             Encoding enc = Encoding.ASCII;
             byte[] bytes = enc.GetBytes(str);
             return bytes;
         }
 
-        public string HexBytesToString(byte[] bytes)
+        public static string HexBytesToString(byte[] bytes)
         {
             string hexString = BitConverter.ToString(bytes);
             return hexString.Replace('-', ' '); // or leave it as is?
