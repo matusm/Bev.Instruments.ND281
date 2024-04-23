a57f6a41f87958db9fd873f2cbeb1c8e43881023
Michael Matus <michael@matus.at>
2024-04-23 22:01:29 +0200

    Make DummyND return random values

--- DummyND.cs
+++ DummyND.cs
@@ -4,12 +4,16 @@ namespace Bev.Instruments.Heidenhain
 {
     public class DummyND : IHeidenhain
     {
+        private readonly Random r;
+
+        public DummyND() => r = new Random();
+
         public string InstrumentManufacturer => "Heidenhain";
 
         public string InstrumentType => "Dummy";
 
         public string InstrumentID => $"{InstrumentManufacturer} {InstrumentType}";
 
-        public double GetValue() => Math.PI;
+        public double GetValue() => r.NextDouble() * 100;
     }
 }
